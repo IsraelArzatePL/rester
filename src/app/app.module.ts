98c4d4a0055bc6f391b54a9f94e20f3355aa1690
IsraelArzatePL <israel.arzate@corepark.com>
2022-10-13 15:50:17 -0500

    Added PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { ServiceWorkerModule } from '@angular/service-worker'
 
 import { AppLayoutComponent } from '@layouts/app-layout'
 
 import { AppComponent } from './app.component'
 import { AppRoutingModule } from './app-routing.module'
+import { environment } from '../environments/environment'
 
 @NgModule({
     declarations: [
@@ -15,7 +17,13 @@ import { AppRoutingModule } from './app-routing.module'
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
-        AppLayoutComponent
+        AppLayoutComponent,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: environment.production,
+            // Register the ServiceWorker as soon as the application is stable
+            // or after 30 seconds (whichever comes first).
+            registrationStrategy: 'registerWhenStable:30000'
+        })
     ],
     providers: [],
     bootstrap: [AppComponent]
